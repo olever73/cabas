dfb07fdaa299f572e1abdee3f98cd9c79a621210
olever <olever73@gmail.com>
2022-07-06 16:02:13 +0200

    add "first "

--- src/main/java/de/teleran/cabas/repository/impl/AreaServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package de.teleran.cabas.repository.impl;
-
-import de.teleran.cabas.service.AreaService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AreaServiceImpl implements AreaService {
-}
---
+++ src/main/java/de/teleran/cabas/service/impl/AreaServiceImpl.java
@@ -0,0 +1,14 @@
+package de.teleran.cabas.service.impl;
+
+import de.teleran.cabas.dto.AreaRequestDTO;
+import de.teleran.cabas.dto.AreaResponseDTO;
+import de.teleran.cabas.service.AreaService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AreaServiceImpl implements AreaService {
+    @Override
+    public AreaResponseDTO create(AreaRequestDTO request) {
+        return null;
+    }
+}
